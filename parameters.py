6bb58b4301b54bce13dde65af7e9ed5a2dd9e007
JoseCouceiro <jose.r.couceiro@gmail.com>
2024-04-25 11:10:07 +0200

    updating april

--- parameters.py
+++ parameters.py
@@ -8,12 +8,13 @@ class Parameters:
         self.__start_date = self.__end_date - timedelta(days=7)
         # self.start_date_str = self.__start_date.strftime('%Y/%m/%d')
         # fixed dates
-        self.start_date_str = '2024/03/01'
+        self.start_date_str = '2024/04/15'
         # self.end_date_str = self.__end_date.strftime('%Y/%m/%d')
         # fixed dates
-        self.end_date_str = '2024/04/01'
+        self.end_date_str = '2024/04/22'
 
-        self.today_str = self.__end_date.strftime('%Y_%m_%d')
+        #self.today_str = self.__end_date.strftime('%Y_%m_%d')
+        self.today_str = '2024_04_22'
         self.day_week = datetime.weekday(self.__end_date)
     
         
